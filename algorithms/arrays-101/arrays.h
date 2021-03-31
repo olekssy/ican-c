d942b53df5572225c1cd41481f0c24075f7da953
Olekssy Lialka <dev.lialka@pm.me>
2021-03-31 16:51:27 +0300

    Added move zeroes to the end of an array

--- algorithms/arrays-101/arrays.h
+++ algorithms/arrays-101/arrays.h
@@ -9,7 +9,7 @@ void print_array(int arr[], int arrSize);
 
 void print_array(int arr[], int arrSize) {
     for (size_t i = 0; i < arrSize; i++) {
-        printf("%d ", arr[i]);
+        printf("%d ", * (arr + i));
     }
     printf("\n");
 }
